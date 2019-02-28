d63b843d3dc8eb8fcbfcc4eab30fa0cff1792389
CRYstaL_Jane <409622462@qq.com>
2019-02-28 20:32:20 +0800

    提交hellogit到本地仓库

---
+++ git-test/src/java/cn/itlcy/git/HelloGit.java
@@ -0,0 +1,10 @@
+package cn.itlcy.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println("CRYstaL_Jane");
+        System.out.println("daydayup");
+
+    }
+}
